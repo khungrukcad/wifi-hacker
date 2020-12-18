75caadabb9fbeb08af75d3f2f3ef3cb9863a2b51
khungrukcad <75356868+khungrukcad@users.noreply.github.com>
2020-12-18 19:57:17 +0700

    Update wifi-hacker.sh
    Add mention moon tacos

--- wifi-hacker.sh
+++ wifi-hacker.sh
@@ -9,7 +9,7 @@
 # WiFi Hacker v2.0
 # esc0rtd3w 2019
 
-# https://github.com/esc0rtd3w/wifi-hacker/
+# https://github.com/khungrukcad/wifi-hacker/
 
 # Uses parts of the aircrack-ng suite, reaver, wifite, and many other tools
 
